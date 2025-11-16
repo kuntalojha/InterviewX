7e89f3e206929e03dbb11df066757fca35e62a62
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-11-16 15:46:25 +0530

    auth and db sync

--- server/src/lib/env.js
+++ server/src/lib/env.js
@@ -7,4 +7,9 @@ export const ENV = {
   PORT: process.env.PORT || 4000,
   DB_URL: process.env.DB_URL || 'mongodb://127.0.0.1:27017/interviewx',
   NODE_ENV: process.env.NODE_ENV || 'development',
-}
+  CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:5173',
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,42 +1,55 @@
 import express from 'express';
 // For connecting frontend and backend
 import path from 'path';
+import cors from 'cors';
+import { serve } from 'inngest/express';
 import { connectDB } from './lib/db.js';
+import { ENV } from './lib/env.js';
 
 const app = express();
-
 
 // Connection Frontend and Backend
 const __dirname = path.resolve();
 
+// Middlewares
+app.use(express.json());
+// creditals: true meaning -> allow cookes to be sent
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
 
+app.use(
+  '/api/inngest',
+  serve({
+    client: inngest,
+    functions,
+  })
+);
 
-app.get("/health", (req, res) => {
-  res.status(200).json({message: "Api is setup and running."})
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Api is setup and running.' });
 });
-app.get("/book", (req, res) => {
-  res.status(200).json({message: "This is the book endpoint."})
+app.get('/book', (req, res) => {
+  res.status(200).json({ message: 'This is the book endpoint.' });
 });
-
 
 // Make our app for production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist","index.html"));
-});
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+  });
 }
-
-
-
 
 const startServer = async () => {
   try {
     await connectDB();
     app.listen(process.env.PORT, () => {
-      console.log(`server is running on http://localhost:${process.env.PORT}`)
+      console.log(`server is running on http://localhost:${process.env.PORT}`);
     });
   } catch (error) {
     console.log('Error starting the server', error);
