29841bf2ce25744bc4f5e545e042ea118762e042
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-11-16 12:57:06 +0530

    update db

--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -2,8 +2,11 @@ import mongoose from 'mongoose';
 import { ENV } from './env.js';
 export const connectDB = async () => {
   try {
-    await mongoose.connect(ENV.DB_URL);
-    console.log('Database connected successfully ✅');
+    if (!ENV.DB_URL) {
+      throw new Error('Database URL is not defined in environment variables');
+    }
+    const conn = await mongoose.connect(ENV.DB_URL);
+    console.log('Database connected successfully ✅', conn.connection.host);
   } catch (error) {
     console.log('Error connecting to MongoDB database ❌', error);
     process.exit(1); // 0 means success, 1 means failure
