9ec1f779b5e17753e6db339fc7bf1654516601d5
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-11-20 02:03:54 +0530

    work on navbar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,33 +1,30 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-} from '@clerk/clerk-react';
-import React from 'react';
-import toast from 'react-hot-toast';
+import React, { Camera } from 'react';
+import { Link } from 'react-router';
+import { HiOutlineSparkles } from 'react-icons/hi2';
 
 function HomePage() {
   return (
-    <div>
-      <button
-        className="btn btn-neutral"
-        onClick={() => toast.success('This is a success message')}
-      >
-        Click Me
-      </button>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Login</button>
-        </SignInButton>
-      </SignedOut>
+    <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
+      {/* NABBAR */}
+      <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
+        <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
+          {/* logo */}
+          <Link
+            to={'/'}
+            className="flex items-center gap-3 hover:scale-105 transition-transform duration-150 "
+          >
+            <div className="size-10 rounded-xl bg-[#057357] flex items-center justify-center shadow-lg">
+              <HiOutlineSparkles className="size-6 text-white" />
+            </div>
 
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-
-      <UserButton />
+            <div className="flex flex-col">
+              <span className="font-black text-xl bg-[#0ae8ad] bg-clip-text text-transparent font-mono tracking-wider">
+                InterviewX
+              </span>
+            </div>
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 }
