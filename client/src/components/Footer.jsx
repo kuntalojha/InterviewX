c5e2de803b9bca9e0237ff2a2f518567386fa49b
Kuntal Ojha <kuntalojha2021@gmail.com>
2025-11-23 23:05:19 +0530

    Enhance Problems Page with problem listing and statistics; add new problems and difficulty badge utility

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,31 @@
 import { HiOutlineSparkles } from 'react-icons/hi2';
 import { FaLinkedin, FaGithub, FaGlobe } from 'react-icons/fa';
-
+import { Link } from 'react-router';
 function Footer() {
   return (
-    <footer className="bg-base-200 border-t border-base-300 mt-10">
+    <footer className="bg-base-200 border-t border-green-900 pt-5">
       <div className="max-w-7xl mx-auto px-4 py-8 flex flex-col gap-8">
         {/* Top Section */}
         <div className="flex flex-col sm:flex-row items-center justify-between gap-6">
           {/* Logo */}
           <div className="flex items-center gap-3">
-            <div className="size-10 rounded-xl bg-[#057357] flex items-center justify-center shadow-lg">
-              <HiOutlineSparkles className="size-6 text-white" />
-            </div>
-            <span className="font-black text-xl bg-[#0ae8ad]/80 bg-clip-text text-transparent font-mono tracking-wider">
-              InterviewX
-            </span>
+            <Link
+              to={'/'}
+              className="flex items-center gap-3 hover:scale-105 transition-transform duration-150 "
+            >
+              <div className="size-10 rounded-xl bg-[#057357] flex items-center justify-center shadow-lg">
+                <HiOutlineSparkles className="size-6 text-white" />
+              </div>
+
+              <div className="flex flex-col">
+                <span className="font-black text-xl bg-[#0ae8ad] bg-clip-text text-transparent font-mono tracking-wider">
+                  InterviewX
+                </span>
+                <span className="text-xs text-base-content/60 font-medium -mt-1">
+                  Your Interview Hub
+                </span>
+              </div>
+            </Link>
           </div>
 
           {/* 🌐 Social Links in Middle */}
